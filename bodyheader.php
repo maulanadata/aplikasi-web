34c17ccbbd96ba8c9faa3d36a2a677c8e5ba86d6
Mohamad Wildan Maulana <63152884+maulanadata@users.noreply.github.com>
2021-12-12 12:35:09 +0700

    Add files via upload

---
+++ bodyheader.php
@@ -0,0 +1,15 @@
+
+<!-- <img src="assets/img/bendera.png" style="height:50px; margin-top:20px; margin-left:10px;" /> -->
+
+<div id="logo-bendera-container">
+	<image id="logo-bendera" src="assets/img/bendera.png" border=0 style="border:0; text-decoration:none; outline:none"> 
+</div>
+
+<div id="nama-alamat-kursus-container">
+	<div class="nama-alamat-kursus">
+		<h2> LPK SORA MIRAI </h2>
+	</div> <br>
+	<div class="nama-alamat-kursus">
+		<h5>Slawi Kab.Tegal Prov.Jateng, HP: +6281234567890</h5>
+	</div>
+</div>
