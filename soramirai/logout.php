3dd2f079b5cc19e6c87f827161813a74ebb72c34
Mohamad Wildan Maulana <63152884+maulanadata@users.noreply.github.com>
2022-05-31 16:40:24 +0700

    Delete soramirai directory

--- soramirai/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['sesi']);
-unset($_SESSION['id_admin']);
-unset($_SESSION['id_user']);
-session_destroy();
-header("Location:index.php?hal=home");
-?>
--- soramirai/pages/home.php
+++
@@ -1,9 +0,0 @@
-<div id="home" class="text-center" style="height:40px;">
-	<h1>SELAMAT DATANG DI LPK SORA MIRAI</h1>
-		<br>
-	<h4> Sebagai lembaga kursus yang menggeluti dunia pelatihan bahasa, kami selalu memberikan pemahaman yang mendalam dengan konsep yang mudah dipahami oleh siswa pemula sekalipun </h4>
-		<br>
-	<h4> Dalam 3 bulan sudah lancar berbicara, mendengar, dan menulis bahasa Jepang setara N5 </h4>
-		<br>
-	<h4> Silahkan mendaftar terlebih dahulu untuk melihat fiur-fitur yang kami berikan kepada anggota dan jangan lupa ikuti penawaran kursus yang kami berikan </h4>
-</div>
